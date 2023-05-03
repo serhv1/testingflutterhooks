c317d4c43011784e5aad65b02c7a071f1fe8e7e8
serhv1 <122094564+serhv1@users.noreply.github.com>
2023-05-03 15:28:09 +0300

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
@@ -6,43 +7,41 @@ void main() {
     title: 'Flutter Demo',
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
-      primarySwatch: Colors.red,
+      primarySwatch: Colors.blue,
       useMaterial3: true,
     ),
     home: const HomePage(),
   ));
 }
 
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
+
+const url = 'https://bit.ly/3HyTOuv';
+
 class HomePage extends HookWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-          return null;
-        });
-      },
-      [controller],
-    );
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)));
+
+    final snapshot = useFuture(future);
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: const Text(
-          'Home Page',
-          style: TextStyle(color: Colors.white),
-        ),
+        title: const Text('Home Page'),
       ),
       body: Column(
-        children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('You typed ${text.value}'),
-        ],
+        children: [snapshot.data].compactMap().toList(),
       ),
     );
   }
